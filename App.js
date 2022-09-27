309c8de667b45358fef0a34538e81f29283ce955
Filipi Rafael <filipirafael.123@gmail.com>
2022-09-27 11:21:33 -0300

    Add new updates

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, PermissionsAndroid, Dimensions, Platform } from 'react-native';
+import MapView from 'react-native-maps';
+import Geolocation from '@react-native-community/geolocation';
+
+const { width, height } = Dimensions.get('screen');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>My first app with React Native!</Text>
-      <StatusBar style="auto" />
+      <MapView
+      onMapReady={() => {
+        Platform.OS === 'android' ? 
+          PermissionsAndroid
+          .request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION)
+          .then(() => console.log('Usuário android aceitou!'))
+        : Permission.request(Permission.PERMISSIONS.ACCESS_FINE_LOCATION)
+          .then(() => console.log('Usuário IOS aceitou!'));
+      }}
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
@@ -18,4 +36,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: width,
+    height: height
+  }
 });
